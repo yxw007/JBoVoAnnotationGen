5f2c88c2e50000ff4c6697e952fdead216d1708d
Potter <75838259@qq.com>
2022-09-09 18:39:36 +0800

    add:support inject import fragment

--- main.js
+++ main.js
@@ -6,33 +6,55 @@ function autoGen(content) {
 
 	function formatFieldName(names) {
 		let ret = "";
-		for (const name of names) {
-			let firstChar = name.substring(0, 1);
-			let surplus = name.substring(1);
-			ret += firstChar.toUpperCase() + surplus;
+		for (let i = 0; i < names.length; i++) {
+			let name = names[i];
+			if (i === 0) {
+				ret += name;
+			} else {
+				let firstChar = name.substring(0, 1);
+				let surplus = name.substring(1);
+				ret += firstChar.toUpperCase() + surplus;
+			}
 		}
 		return ret;
 	}
 
-	return content.replace(/^\s+private.*;/gm, (match) => {
-		if (!match) {
-			return match;
-		}
-		let arr = match.trim().split(" ");
-		if (arr.length < 3) {
-			return match;
-		}
+	function convertImport(content) {
+		let res = content.replace(/(public class)/gmi, (match) => {
+			const importCode = "import com.alibaba.fastjson.annotation.JSONField;" +
+				+ "import com.fasterxml.jackson.annotation.JsonProperty;"
+				+ "import io.swagger.annotations.ApiModelProperty;"
+				+ `import lombok.Data;\n\n@Data\n${match}`
+			return importCode;
+		});
+		return res;
+	}
 
-		let variableName = arr.pop().slice(0, -1);
+	function convertField(content) {
+		let res = content.replace(/^\s+private.*;/gm, (match) => {
+			let arr = match.trim().split(" ");
+			if (arr.length < 3) {
+				return match;
+			}
 
-		let fieldName = formatFieldName(variableName.split("_"));
-		arr.push(fieldName);
+			let variableName = arr.pop().slice(0, -1);
 
-		let fragment = arr.join(" ");
-		let res = `    @ApiModelProperty("")\n    @JsonProperty("${variableName}")\n    @JSONField(name="${variableName}")\n    ${fragment};`
+			let fieldName = formatFieldName(variableName.split("_"));
+			arr.push(fieldName);
+
+			let fragment = arr.join(" ");
+			let res = `    @ApiModelProperty("")\n    @JsonProperty("${variableName}")\n    @JSONField(name="${variableName}")\n    ${fragment};\n`
+
+			return res;
+		})
 
 		return res;
-	})
+	}
+
+	let convert1 = convertImport(content);
+	let convert2 = convertField(convert1);
+
+	return convert2;
 }
 
 let result = autoGen(content);
